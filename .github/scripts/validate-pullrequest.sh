291e3990ced2a7ed3e11253af334371faadc841a
Chris Werner Rau <cwrau@cwrcoding.com>
2022-06-10 14:59:38 +0200

    feat: Add pullrequest validation feat: use ct for linting

---
+++ .github/scripts/validate-pullrequest.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -exu
+set -o pipefail
+
+: "${PR_TITLE:?Environment variable must be set}"
+
+local repo_root
+repo_root=$(git rev-parse --show-toplevel)
+
+local changed
+changed=$(ct list-changed --config "$repo_root/ct.yaml")
+
+if [[ -z "$changed" ]]; then
+    exit 0
+fi
+
+local num_changed
+num_changed=$(wc -l <<< "$changed")
+
+if ((num_changed > 1)); then
+    echo "This PR has changes to multiple charts. Please create individual PRs per chart." >&2
+    exit 1
+fi
+
+# Strip charts directory
+changed="${changed##*/}"
+
+if [[ "$PR_TITLE" != "[$changed] "* ]]; then
+    echo "PR title must start with '[$changed] '." >&2
+    exit 1
+fi
