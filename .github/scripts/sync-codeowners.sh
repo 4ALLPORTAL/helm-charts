bb93b645ecee33371d2dc14adc2270d31dc31993
Chris Werner Rau <cwrau@cwrcoding.com>
2022-06-13 13:49:14 +0200

    fix: Sort CODEOWNERS entries

--- .github/scripts/sync-codeowners.sh
+++ .github/scripts/sync-codeowners.sh
@@ -7,4 +7,5 @@ for DIR in ./charts/*; do
   DIR="${DIR//\./}"
   MAINTAINERS="$(yq e '.maintainers.[].name' "$FILE" | sed 's/^/@/' | sort --ignore-case | tr '\r\n' ' ')"
   echo -e "$DIR/ $MAINTAINERS"
-done
+done | sort
+
